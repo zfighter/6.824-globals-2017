acd0b2a29dd49bd21ab5cd742fe8aa9fd24b619c
zfighter <fidelzheng@gmail.com>
2017-04-27 22:38:02 +0800

    update shell

--- main/ii.go
+++ main/ii.go
@@ -2,7 +2,7 @@ package main
 
 import "os"
 import "fmt"
-import "mapreduce"
+import "github.com/6.824/mapreduce"
 
 // The mapping function is called once for each piece of the input.
 // In this framework, the key is the name of the file that is being processed,
--- main/test-mr.sh
+++ main/test-mr.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 here=$(dirname "$0")
 [[ "$here" = /* ]] || here="$PWD/$here"
-export GOPATH="$here/../../"
+#export GOPATH="$here/.."
+echo $GOPATH
 echo ""
 echo "==> Part I"
-go test -run Sequential mapreduce/...
+go test -run Sequential github.com/6.824/mapreduce/...
 echo ""
 echo "==> Part II"
 (cd "$here" && sh ./test-wc.sh > /dev/null)
 echo ""
 echo "==> Part III"
-go test -run TestBasic mapreduce/...
+go test -run TestBasic github.com/6.824/mapreduce/...
 echo ""
 echo "==> Part IV"
-go test -run Failure mapreduce/...
+go test -run Failure github.com/6.824/mapreduce/...
 echo ""
 echo "==> Part V (challenge)"
 (cd "$here" && sh ./test-ii.sh > /dev/null)
