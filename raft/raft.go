d10a4e10cbddb1d1fe916485c6d7fb42d9f93e25
zfighter <fidel.zheng@gmail.com>
2017-12-11 20:09:40 +0800

    begin 2C

--- raft/raft.go
+++ raft/raft.go
@@ -100,13 +100,14 @@ func (rf *Raft) GetState() (int, bool) {
 //
 func (rf *Raft) persist() {
 	// Your code here (2C).
-	// Example:
-	// w := new(bytes.Buffer)
-	// e := gob.NewEncoder(w)
-	// e.Encode(rf.xxx)
-	// e.Encode(rf.yyy)
-	// data := w.Bytes()
-	// rf.persister.SaveRaftState(data)
+	w := new(bytes.Buffer)
+	e := gob.NewEncoder(w)
+	e.Encode(rf.voteFor)
+	e.Encode(rf.currentTerm)
+	lastIndex := len(rf.logs) - 1
+	e.Encode(rf.logs[lastIndex])
+	data := w.Bytes()
+	rf.persister.SaveRaftState(data)
 }
 
 //
@@ -114,7 +115,6 @@ func (rf *Raft) persist() {
 //
 func (rf *Raft) readPersist(data []byte) {
 	// Your code here (2C).
-	// Example:
 	// r := bytes.NewBuffer(data)
 	// d := gob.NewDecoder(r)
 	// d.Decode(&rf.xxx)
@@ -122,6 +122,8 @@ func (rf *Raft) readPersist(data []byte) {
 	if data == nil || len(data) < 1 { // bootstrap without any state?
 		return
 	}
+	r := bytes.NewBuffer(data)
+	d := gob.NewDecoder(r)
 }
 
 // AppendEntry RPC
