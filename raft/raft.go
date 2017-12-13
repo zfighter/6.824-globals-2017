c33af44b764df2f18f202e6b0cea08fd73953ef1
zfighter <fidel.zheng@gmail.com>
2017-12-13 20:26:39 +0800

    to support append logs in a batch when it is recovering.

--- raft/raft.go
+++ raft/raft.go
@@ -100,14 +100,14 @@ func (rf *Raft) GetState() (int, bool) {
 //
 func (rf *Raft) persist() {
 	// Your code here (2C).
-	w := new(bytes.Buffer)
-	e := gob.NewEncoder(w)
-	e.Encode(rf.voteFor)
-	e.Encode(rf.currentTerm)
-	lastIndex := len(rf.logs) - 1
-	e.Encode(rf.logs[lastIndex])
-	data := w.Bytes()
-	rf.persister.SaveRaftState(data)
+	// w := new(bytes.Buffer)
+	// e := gob.NewEncoder(w)
+	// e.Encode(rf.voteFor)
+	// e.Encode(rf.currentTerm)
+	// lastIndex := len(rf.logs) - 1
+	// e.Encode(rf.logs[lastIndex])
+	// data := w.Bytes()
+	// rf.persister.SaveRaftState(data)
 }
 
 //
@@ -122,8 +122,6 @@ func (rf *Raft) readPersist(data []byte) {
 	if data == nil || len(data) < 1 { // bootstrap without any state?
 		return
 	}
-	r := bytes.NewBuffer(data)
-	d := gob.NewDecoder(r)
 }
 
 // AppendEntry RPC
@@ -132,15 +130,15 @@ type AppendEntryArgs struct {
 	LeaderId     int
 	PrevLogIndex int
 	PrevLogTerm  int
-	// LogTerm      int
-	// LogCommand   interface{}
 	Entry        LogEntry
 	LeaderCommit int
 }
 
 type AppendEntryReply struct {
-	Term    int
-	Success bool
+	Term         int
+	Success      bool
+	ConflictTerm int
+	FirstIndex   int
 }
 
 func (rf *Raft) AppendEntry(args *AppendEntryArgs, reply *AppendEntryReply) {
@@ -170,6 +168,16 @@ func (rf *Raft) AppendEntry(args *AppendEntryArgs, reply *AppendEntryReply) {
 		fmt.Printf("Args: PrevLogTerm=%d, PrevLogIndex=%d; local: PrevLogIndex=%d\n", args.PrevLogTerm, args.PrevLogIndex, len(rf.logs)-1)
 		reply.Term = localTerm
 		reply.Success = false
+		if args.PrevLogIndex < len(rf.logs) {
+			conflictTerm := rf.logs[args.PrevLogIndex].Term
+			reply.ConflictTerm = conflictTerm
+			//TODO: change to binary search later
+			for i := args.PrevLogIndex - 1; i > 0; i-- {
+				if rf.logs[i].Term != conflictTerm {
+					reply.FirstIndex = i + 1
+				}
+			}
+		}
 		return
 	}
 	// 2.update time. in order to present too much invalid heartbeat,
@@ -632,6 +640,10 @@ func (rf *Raft) appendToServer(server int, currentIndex int, request *AppendEntr
 			fmt.Printf("Peer-%d received reply from peer-%d, it's term=%d is larger than currentTerm=%d\n", rf.me, server, reply.Term, rf.currentTerm)
 			rf.currentTerm = reply.Term
 			rf.isLeader = false
+		} else if reply.ConflictTerm != 0 {
+			if rf.logs[reply.FirstIndex].Term != reply.ConflictTerm {
+				rf.nextIndex[server] = reply.FirstIndex
+			}
 		}
 		rf.mu.Unlock()
 	}
