0202f0ba17e42ab55dca3b66a10c8babf7df60f5
zfighter <fidel.zheng@gmail.com>
2018-04-04 19:30:48 +0800

    20180404, todo: add logger.

--- raft/util.go
+++ raft/util.go
@@ -1,13 +1,34 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 // Debugging
 const Debug = 1
+const Truncate = true
+const fileName = "debug.log"
+
+func Init() {
+	if _, statErr := os.Stat(fileName); !os.IsNotExist(statErr) && Truncate {
+		rmErr := os.Remove(fileName)
+		if rmErr != nil {
+			log.Printf("%s\n", rmErr.Error())
+		}
+	}
+	logFile, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	logger := log.New(logFile, "", log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
-		log.Printf(format, a...)
+		fileName := "debug.log"
+		if err != nil {
+			log.Printf("%s\n", err.Error())
+			os.Exit(-1)
+		}
+		logger.Printf(format, a...)
 	}
 	return
 }
